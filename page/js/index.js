11216726435ae170ed1a825b2aa8f63b5de72d75
Flyher D <flyher@users.noreply.github.com>
2019-04-18 16:43:05 +0800

    source for blog

---
+++ page/js/index.js
@@ -0,0 +1,16 @@
+// 404 Error page
+//Responsive: Large desktop (1800px), Standard, Mobile (568px)
+// if (window.location.protocol.toLowerCase() === 'http:' && window.location.hostname.toLowerCase() !== 'localhost') {
+//     window.location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+// }
+/* eslint-disable */
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+        m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-65955218-1', 'auto');
+ga('send', 'pageview');
+/* eslint-disable */
